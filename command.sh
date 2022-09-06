105fa93b2cc9b876fd37098e0571fd214ae63e9a
Alston <alstondsouza00@gmail.com>
2022-09-06 22:43:15 +0530

    listed all the commands used

---
+++ command.sh
@@ -0,0 +1,15 @@
+# REPO_URL https://github.com/Blue-78/reactapp.git
+# creating reactapp using react_app tool
+# command used: nx create-react_app myapp
+# command to initialize git: git init
+# command used for creating repo: gh repo create
+# created empty folder and added locally: git add <folder_name>
+# command used for Creating empty Git repository on remote server: git remote add origin https://github.com/Blue-78/reactapp.git 
+# creating default 'main' branch: git branch -M main
+# creating first commit: git commit -m "message"
+# creating tracking reference for all branches and push files to remote 'main' branch in this case: git push -u origin main
+# cloned the remote repo to add new folders: git clone https://github.com/Blue-78/reactapp.git
+# created new branch which is same as the 'main' branch: git branch upload_logo
+# replaced the logo and added a link to file and added commit: App.js
+# created PR request in upload_logo branch: gh pr create
+# merged the branch while keepingn the commit at the same time: gh pr merge
